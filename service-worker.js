23dd2880ec6003c5b884f58135f3009022dfaf7d
nykangtest <nayoung.kang@gmail.com>
2016-03-07 15:01:39 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -67,10 +67,11 @@ self.addEventListener('activate', function(event) {
       // This will trigger navigator.serviceWorker.onmessage in each client.
       //return self.clients.matchAll().then(function(clients) {
       return self.clients.matchAll({type: 'wearable'}).then(function(clients) {
-        return Promise.all(clients.map(function(client) {
+       // return Promise.all(clients.map(function(client) {
+        clients.forEach(function(client) {
           return client.postMessage('The service worker has activated and ' +
             'taken control.');
-        }));
+        };
       });
     })
   );
